6c17a946c1123bcb493bd67531d922c1bbdc0aea
danikk812 <shushambin@mail.ru>
2020-12-22 20:39:41 +0300

    commit

--- src/main/java/RegistrationServlet.java
+++ src/main/java/RegistrationServlet.java
@@ -13,19 +13,19 @@ public class RegistrationServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        String login  = req.getParameter("login");
+        String login = req.getParameter("login");
         String password = req.getParameter("password");
-
-        if (name.length() < 1 || login.length() < 3 || password.length() < 3) {
-            resp.getWriter().println("Too short name, login or password");
-        }
 
         if (inMemoryUserStorage.users.size() > 0 && inMemoryUserStorage.contains(login)) {
             resp.getWriter().println("User with such login already exists");
         }
 
-        User user = new User(name, login, password);
-        inMemoryUserStorage.save(user);
-        resp.getWriter().println("You are successfully registered!");
+        if (name.length() > 0 && login.length() > 2 && password.length() > 2) {
+            User user = new User(name, login, password);
+            inMemoryUserStorage.save(user);
+            resp.getWriter().println("You are successfully registered!");
+        } else {
+            resp.getWriter().println("Too short name, login or password!");
+        }
     }
 }
